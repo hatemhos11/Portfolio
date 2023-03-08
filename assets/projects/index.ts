fe76a3733610bb226027fdddd44bb7f5f0091199
hatemhos11 <hatemhosni00@gmail.com>
2023-03-09 00:50:27 +0200

    fix button bug

--- assets/projects/index.ts
+++ assets/projects/index.ts
@@ -10,29 +10,30 @@ export {default as project1_1} from './project1/1.png'
 export {default as project1_2} from './project1/2.png'
 export {default as project1_3} from './project1/3.png'
 // // 2
-// export {default as project2_1} from './project2/1.png'
-// export {default as project2_2} from './project2/2.png'
-// export {default as project2_3} from './project2/3.png'
-// // 3
-// export {default as project3_1} from './project3/1.png'
-// export {default as project3_2} from './project3/2.png'
-// export {default as project3_3} from './project3/3.png'
-// // 4
-// export {default as project4_1} from './project4/1.png'
-// export {default as project4_2} from './project4/2.png'
-// export {default as project4_3} from './project4/3.png'
-// // 5
-// export {default as project5_1} from './project5/1.png'
-// export {default as project5_2} from './project5/2.png'
-// export {default as project5_3} from './project5/3.png'
-// // 6
-// export {default as project6_1} from './project6/1.png'
-// export {default as project6_2} from './project6/2.png'
-// export {default as project6_3} from './project6/3.png'
-// // 7
-// export {default as project7_1} from './project7/1.png'
-// export {default as project7_2} from './project7/2.png'
-// export {default as project7_3} from './project7/3.png'
+export {default as project2_1} from './project2/1.png'
+export {default as project2_2} from './project2/2.png'
+// 3
+export {default as project3_1} from './project3/1.png'
+export {default as project3_2} from './project3/2.png'
+export {default as project3_3} from './project3/3.png'
+export {default as project3_4} from './project3/4.png'
+// 4
+export {default as project4_1} from './project4/1.png'
+// 5
+export {default as project5_1} from './project5/1.png'
+export {default as project5_2} from './project5/2.png'
+export {default as project5_3} from './project5/3.png'
+export {default as project5_4} from './project5/4.png'
+// 6
+export {default as project6_1} from './project6/1.png'
+export {default as project6_2} from './project6/2.png'
+export {default as project6_3} from './project6/3.png'
+export {default as project6_4} from './project6/4.png'
+export {default as project6_5} from './project6/5.png'
+// 7
+export {default as project7_1} from './project7/1.png'
+export {default as project7_2} from './project7/2.png'
+export {default as project7_3} from './project7/3.png'
 // // 8
 // export {default as project8_1} from './project8/1.png'
 // export {default as project8_2} from './project8/2.png'
